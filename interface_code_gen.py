5b751e2b97fd9672ae63a660a5c8f9dd8be2b9ae
Dan Lazewatsky <dan@lazewatsky.com>
2012-12-11 16:11:31 -0600

    more complete

--- interface_code_gen.py
+++ interface_code_gen.py
@@ -3,15 +3,23 @@ import pickle
 import sys
 import hashlib
 import pystache
+import roslib; roslib.load_manifest('projected_interface_builder')
+from projected_interface_builder.convert_utils import QtPolyToROS
+from PySide.QtCore import QPoint
 
-header = '''
-#!/usr/bin/env python
-import roslib; roslib.load_manifest(PKG_NAME_HERE)
+header = '''#!/usr/bin/env python
+# FIXME this package name is just for testing
+import roslib; roslib.load_manifest('projected_interface_builder')
 import rospy
-from projector_interface.srv import GetCursorStats, DrawPolygon, CircleInhibit
+from projector_interface.srv import GetCursorStats, DrawPolygon, DrawPolygonRequest
+from geometry_msgs.msg import Point, PolygonStamped
+from projected_interface_builder.colors import *
 
 class ProjectedInterface(object):
-    
+    def __init__(self):
+        self.initPolygons()
+        self.start()
+        
     def start(self):
     	self.polygon_proxy = rospy.ServiceProxy('/draw_polygon', DrawPolygon)
     	rospy.loginfo("Waiting for polygon service")
@@ -26,12 +34,14 @@ class ProjectedInterface(object):
         self.publish_polygons()
     
     def publish_polygons(self):
-        
+        for polygon in self.polygons.itervalues():
+            self.polygon_proxy(polygon)
+         
     
     def dispatch(self, msg):
         poly_name = msg.data
         poly_hash = hashlib.md5(poly_name).hexdigest()
-        self.__getattribute__('_%s' % poly_hash).__call__()
+        self.__getattribute__('_%s' % poly_hash).__call__(self.polygons['poly_name'])
     
 {{functions}}
 
@@ -39,13 +49,24 @@ class ProjectedInterface(object):
 if __name__ == '__main__':
     rospy.init_node('projected_interface')
     interface = ProjectedInterface()
-    interface.start()
 '''
 
 func_template = '''
     # callback for {{funcname}}
-    def _{{funchash}}(self):
+    def _{{funchash}}(self, polygon):
         pass
+'''
+
+polygon_template = '''
+        self.polygons['{{name}}'] = DrawPolygonRequest(
+            '{{name}}',
+            True,
+            PolygonStamped(
+                header=rospy.Header(frame_id='{{frame_id}}'),
+            ),
+            WHITE
+        )
+        self.polygons['{{name}}'].polygon.polygon.points = [{{points}}]
 '''
 
 def load(fname):
@@ -62,10 +83,24 @@ if __name__ == '__main__':
     # _, infile, outfile = sys.argv
     # _, infile, outfile = sys.argv
     data = load('physical_copy.pkl')
+    res = float(data['resolution'])
     functions = ''
+    polygons = '''
+    def initPolygons(self):
+        self.polygons=dict()'''
     for name, poly in data['polygons'].iteritems():
-        functions += pystache.render(func_template, name_dict(name))
+        namedict = name_dict(name)
+        functions += pystache.render(func_template, namedict)
+        points = ','.join(['Point(%s,%s,%s)' % (p.x()*res+float(data['offset_x']),p.y()*res+float(data['offset_y']),data['offset_z']) for p in poly])
+        polygons  += pystache.render(
+            polygon_template,
+            dict(
+                name=name.replace('\n', '\\n'),
+                frame_id=data['frame_id'],
+                points=points
+            )
+        )
 
     print pystache.render(header, dict(
-        functions = functions
+        functions = polygons + functions
     ))
