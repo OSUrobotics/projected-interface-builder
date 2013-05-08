b255683449ce1cbbfb9e5307c08eb54206af4b55
Dan Lazewatsky <dan@lazewatsky.com>
2013-05-08 14:57:21 -0500

    documentation and code cleanup

--- src/projected_interface_builder/projected_interface.py
+++ src/projected_interface_builder/projected_interface.py
@@ -42,12 +42,24 @@ from dynamic_reconfigure.server import Server
 from projected_interface_builder.cfg import InterfaceConfig
 
 class ProjectedInterface(object):
-    callbacks = dict()
-    dispatch_lock = RLock()
-    config_inited = False
-    save_changes = False
-    hidden = set()
+    '''
+    Base class for all projected interfaces. Automatically starts up service
+    proxies for sending and clearing polygons, as well as a dynamic_reconfigure
+    server for configuring the node, and a subscription for clicks. 
+    '''
     def __init__(self, polygon_file, dispatch_rate=0.1):
+        '''
+        ProjectedInterface constructor.
+
+        @param polygon_file: pickle-formatted file path containing interface data
+        @param dispatch_rate:  not currently used
+        '''
+        self._callbacks = dict()
+        self._dispatch_lock = RLock()
+        self._config_inited = False
+        self._save_changes = False
+        self._hidden = set()
+
         with open(polygon_file, 'r') as f:
             data = pickle.load(f)
         self.polygon_file = polygon_file
@@ -60,6 +72,10 @@ class ProjectedInterface(object):
         self.dispatch_rate = dispatch_rate
         
     def start(self):
+        '''
+        Initializes all of the service proxies, subscribers and publishers.
+        Also publishes all of the polygons.
+        '''
     	self.polygon_proxy = rospy.ServiceProxy('/draw_polygon', DrawPolygon)
     	rospy.loginfo("Waiting for polygon service")
     	self.polygon_proxy.wait_for_service()
@@ -68,70 +84,84 @@ class ProjectedInterface(object):
         self.polygon_clear_proxy = rospy.ServiceProxy('/clear_polygons', ClearPolygons)
         rospy.loginfo("polygon clear service ready")
     	self.polygon_viz = rospy.Publisher('/polygon_viz', PolygonStamped)
-        self.subscribe_to_clicks()
+        self._subscribe_to_clicks()
         self.dispatch_rate = rospy.Rate(self.dispatch_rate)
-        reconfig_srv = Server(InterfaceConfig, self.reconfig_cb)
+        reconfig_srv = Server(InterfaceConfig, self._reconfig_cb)
 
         self.publish_polygons()
 
-    def reconfig_cb(self, config, level):
+    def _reconfig_cb(self, config, level):
+        '''Callback for dynamic reconfigure. Don't overwrite this'''
         # ignore the first config we get
-        # import pdb; pdb.set_trace()
-        if self.config_inited:
+        if self._config_inited:
             self.frame_id = config['frame_id']
             self.res = config['res']
             self.x = config['offset_x']
             self.y = config['offset_y']
             self.z = config['offset_z']
-            self.save_changes = config['commit']
+            self._save_changes = config['commit']
         else:
             config['groups']['parameters']['frame_id'] = self.frame_id
             config['res'] = self.res
             config['offset_x'] = self.x
             config['offset_y'] = self.y
             config['offset_z'] = self.z
-            self.config_inited = True
+            self._config_inited = True
         self.publish_polygons()
 
         return config
 
-    def subscribe_to_clicks(self):
-        self.click_sub = rospy.Subscriber('/clicked_object', std_msgs.msg.String, self.dispatch, queue_size=1)
+    def _subscribe_to_clicks(self):
+        self.click_sub = rospy.Subscriber('/clicked_object', std_msgs.msg.String, self._dispatch, queue_size=1)
         
-    def dispatch(self, msg):
-        if self.dispatch_lock.acquire(blocking=False) is True:
+    def _dispatch(self, msg):
+        '''Receives a click and calls any callbacks defined for the specified object.'''
+        if self._dispatch_lock.acquire(blocking=False) is True:
             uid = msg.data
-            if uid in self.callbacks:
-                self.callbacks[uid].__call__(self.polygons[uid])
-            self.dispatch_lock.release()
+            if uid in self._callbacks:
+                self._callbacks[uid].__call__(self.polygons[uid])
+            self._dispatch_lock.release()
         
     def register_callback(self, uid, func):
-        self.callbacks[uid] = func
+        '''
+        Registers a callback for a particular polygon.
+
+        @param uid: Unique ID of a polygon
+        @param func: Callback function. Should take a polygon object as an argument
+        '''
+        self._callbacks[uid] = func
 
     def publish_polygon(self, polygon):
+        '''Publishes a single polygon.'''
         ps = QtPolyToROS(polygon.polygon, polygon.id, self.x, self.y, self.z, self.res, self.frame_id)
         ps.header.stamp = rospy.Time.now()
         text_rect = QtPolyToROS(QtRectToPoly(polygon.text_rect), '', self.x, self.y, self.z, self.res, self.frame_id)
         self.polygon_proxy(polygon.id, polygon.name, ps, text_rect.polygon, colors.WHITE)
             
     def set_hidden(self, polygon):
-        self.hidden.add(polygon)
+        '''Hides a polygon. Has no effect if the polygon is already hidden.'''
+        self._hidden.add(polygon)
 
     def set_nothidden(self, polygon):
-        if polygon in self.hidden:
-            self.hidden.remove(polygon)
+        '''Unhides a polygon. Has no effect if the polygon is not hidden.'''
+        if polygon in self._hidden:
+            self._hidden.remove(polygon)
 
     def clear_hidden(self):
-        self.hidden.clear()
+        '''Unhides all polygons.'''
+        self._hidden.clear()
 
     def publish_polygons(self):
+        '''Publishes all polygons.'''
         self.polygon_clear_proxy()
         for uid, polygon in self.polygons.iteritems():
-            if uid not in self.hidden:
+            if uid not in self._hidden:
                 self.publish_polygon(polygon)
 
     def maybe_write_changes(self):
-        if self.save_changes:
+        '''Writes configuration changes to the interface file.'''
+        # TODO: call this in __del__
+        if self._save_changes:
             with open(self.polygon_file, 'w') as f:
                 data = dict()
                 data['resolution'] = self.res
